6fdfcdee9badab6e822671d41446569a805c6374
Henrique Gomes <oi.henriquegomes@gmail.com>
2023-08-16 21:14:59 -0300

    Criando métodos de consulta

--- Trilha back-end/JPA_Hibernate/lojamusique/src/main/java/br/com/lojamusique/DAO/ProdutoDao.java
+++ Trilha back-end/JPA_Hibernate/lojamusique/src/main/java/br/com/lojamusique/DAO/ProdutoDao.java
@@ -3,6 +3,8 @@ package br.com.lojamusique.DAO;
 import br.com.lojamusique.model.Produto;
 
 import javax.persistence.EntityManager;
+import java.math.BigDecimal;
+import java.util.List;
 
 public class ProdutoDao {
 
@@ -14,5 +16,29 @@ public class ProdutoDao {
 
     public void cadastrar(Produto produto) {
         this.em.persist(produto);
+    }
+
+    public Produto buscarPorId(Long id) {
+        return em.find(Produto.class, id);
+    }
+
+    public List<Produto> buscarTodos(){
+        String jpql = "SELECT p FROM Produto p"; // usar o nome da entidade invés do nome da tabela
+        return em.createQuery(jpql, Produto.class).getResultList();
+    }
+
+    public List<Produto> buscarPorNome(String nome){
+        String jpql = "SELECT p FROM Produto p WHERE p.nome = :nome"; // named parameter, poderia ser ?1, ?2 e aí se referenciaria ao parâmetro pelo número
+        return em.createQuery(jpql, Produto.class).setParameter("nome", nome).getResultList();
+    }
+
+    public BigDecimal buscarPrecoDoProdutoComNome(String nome) {
+        String jpql = "SELECT p.preco FROM Produto p WHERE p.nome = :nome";
+        return em.createQuery(jpql, BigDecimal.class).setParameter("nome", nome).getSingleResult();
+    }
+
+    public List<Produto> buscarPorNomeDaCategoria(String nome){
+        String jpql = "SELECT p FROM Produto p WHERE p.categoria.nome = :nome";
+        return em.createQuery(jpql, Produto.class).setParameter("nome", nome).getResultList();
     }
 }
--- Trilha back-end/JPA_Hibernate/lojamusique/src/test/java/CadastroDeProduto.java
+++ Trilha back-end/JPA_Hibernate/lojamusique/src/test/java/CadastroDeProduto.java
@@ -5,11 +5,35 @@ import br.com.lojamusique.model.Categoria;
 import br.com.lojamusique.model.Produto;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import java.math.BigDecimal;
+import java.util.List;
 
 public class CadastroDeProduto {
 
     public static void main(String[] args) {
+        //cadastrarProduto();
+        EntityManager em = JPAUtil.getEntityManager();
+        ProdutoDao produtoDao = new ProdutoDao(em);
+
+        Produto p = produtoDao.buscarPorId(1L);
+        System.out.println(p.getPreco());
+
+        //List<Produto> todos = produtoDao.buscarTodos();
+        //todos.forEach(produto -> System.out.println(p.getNome()));
+
+        //List<Produto> produto = produtoDao.buscarPorNome("Xiaomi Redmi");
+        //produto.forEach(p2 -> System.out.println(p2.getNome()));
+
+        List<Produto> produto = produtoDao.buscarPorNomeDaCategoria("Celulares");
+        produto.forEach(p2 -> System.out.println(p2.getNome()));
+
+        BigDecimal precoDoProduto = produtoDao.buscarPrecoDoProdutoComNome("Xiaomi Redmi");
+        System.out.println("Preco do produto: " + precoDoProduto);
+
+    }
+
+    private static void cadastrarProduto() {
         Categoria celulares = new Categoria("CELULARES");
 
         Produto celular = new Produto("Motorola E5", "4gb tela led", new BigDecimal("1800"), celulares);
