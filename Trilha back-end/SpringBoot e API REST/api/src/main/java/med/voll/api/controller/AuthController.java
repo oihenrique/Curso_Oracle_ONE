7ea129991ba7f5b2cde1abaf86dff7290a725f69
Henrique Gomes <oi.henriquegomes@gmail.com>
2023-09-18 11:29:12 -0300

    add: implementando geração e autenticação por token jwt

--- Trilha back-end/SpringBoot e API REST/api/src/main/java/med/voll/api/controller/AuthController.java
+++ Trilha back-end/SpringBoot e API REST/api/src/main/java/med/voll/api/controller/AuthController.java
@@ -1,7 +1,10 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
+import med.voll.api.domain.usuario.Usuario;
 import med.voll.api.domain.usuario.dadosAutenticacao;
+import med.voll.api.infra.security.DadosTokenJWT;
+import med.voll.api.infra.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -18,12 +21,16 @@ public class AuthController {
     @Autowired
     private AuthenticationManager manager;
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody @Valid dadosAutenticacao dados) {
 
-        var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
-        var authentication = manager.authenticate(token);
+        var authenticationToken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+        var authentication = manager.authenticate(authenticationToken);
 
-        return ResponseEntity.ok().build();
+        String tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+        return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
     }
 }
