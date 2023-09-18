ebea1d15a9c93efb640eba4857084ecba5d98907
Henrique Gomes <oi.henriquegomes@gmail.com>
2023-09-18 15:10:21 -0300

    add: configurando a autenticação por token jwt para usuários logados

--- Trilha back-end/SpringBoot e API REST/api/src/main/java/med/voll/api/controller/MedicosController.java
+++ Trilha back-end/SpringBoot e API REST/api/src/main/java/med/voll/api/controller/MedicosController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -61,6 +62,7 @@ public class MedicosController {
     }
 
     @GetMapping("/{id}")
+    @Secured("ROLE_ADMIN")
     @Transactional
     public ResponseEntity detalhar(@PathVariable Long id) {
         Medico medico = repository.getReferenceById(id);
